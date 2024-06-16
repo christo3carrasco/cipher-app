f641f04635e73fc80681a52f4179cd25e6b5e768
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-06-16 00:47:48 -0500

    feat: add config enviroments, http, forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserSignInComponent } from './components/user-module/user-sign-in/user-sign-in.component';
+import { UserSignUpComponent } from './components/user-module/user-sign-up/user-sign-up.component';
+import { UserNotFoundComponent } from './components/user-module/user-not-found/user-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: UserSignInComponent },
+  { path: 'sign-up', component: UserSignUpComponent },
+  { path: '**', component: UserNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from './components/shared/angular-material/angular-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
+import { UserSignInComponent } from './components/user-module/user-sign-in/user-sign-in.component';
+import { UserSignUpComponent } from './components/user-module/user-sign-up/user-sign-up.component';
+import { UserNotFoundComponent } from './components/user-module/user-not-found/user-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideAnimationsAsync()],
+  declarations: [
+    AppComponent,
+    UserSignInComponent,
+    UserSignUpComponent,
+    UserNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    { provide: MAT_DATE_LOCALE, useValue: 'es-PE' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
