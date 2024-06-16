661b5dcd555c8ae53e329007c036e8cc7f414231
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-06-16 11:58:37 -0500

    feat: add page not found component

--- src/app/components/user-module/user-not-found/user-not-found.component.ts
+++ src/app/components/user-module/user-not-found/user-not-found.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-not-found',
   templateUrl: './user-not-found.component.html',
-  styleUrl: './user-not-found.component.css'
+  styleUrl: './user-not-found.component.css',
 })
 export class UserNotFoundComponent {
+  constructor(private router: Router) {}
 
+  goToHome() {
+    this.router.navigate(['/']);
+  }
 }
