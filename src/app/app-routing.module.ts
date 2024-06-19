cd9dec8bfc7251e878f7662b33fb9474220bb266
Christopher Carrasco <christopher3carrasco@gmail.com>
2024-06-19 03:19:30 -0500

    feat: add guard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,21 @@ import { UserSignUpComponent } from './components/user-module/user-sign-up/user-
 import { UserNotFoundComponent } from './components/user-module/user-not-found/user-not-found.component';
 import { userGuard } from './services/guard/user.guard';
 import { VoterComponent } from './components/voter-module/voter/voter.component';
+import { VotingComponent } from './components/voting-module/voting/voting.component';
+import { ProfileComponent } from './components/user-module/profile/profile.component';
+import { adminGuard } from './services/guard/admin.guard';
+import { AdminComponent } from './components/admin-module/admin/admin.component';
+import { UserComponent } from './components/admin-module/user/user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: UserSignInComponent },
   { path: 'sign-up', component: UserSignUpComponent },
   { path: 'voter', canActivate: [userGuard], component: VoterComponent },
+  { path: 'voting', canActivate: [userGuard], component: VotingComponent },
+  { path: 'profile', canActivate: [userGuard], component: ProfileComponent },
+  { path: 'admin', canActivate: [adminGuard], component: AdminComponent },
+  { path: 'user', canActivate: [adminGuard], component: UserComponent },
   { path: '**', component: UserNotFoundComponent },
 ];
 
